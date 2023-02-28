b5244999745e0e1551ec3f00a58b45650a43a7ef
“norouzy” <mohamadnewday@gmail.com>
2023-02-28 19:48:44 +0330

    mobile nav clicking

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 const yearEl = document.querySelector(".year-copyright");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("open");
+});
